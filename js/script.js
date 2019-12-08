c379f539c934ed46b675f8fb70bdda94bd2aeeb6
Jevgeni Barkin <zekaxdpro@gmail.com>
2019-12-08 11:11:17 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,6 @@
-var words = document.querySelectorAll(".word");
-words.forEach(function (word) {
-    var letters = word.textContent.split("");
-    word.textContent = "";
-    letters.forEach(function (letter) {
-        var span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
-    });
-});
-var currentWordIndex = 0;
-var maxWordIndex = words.length - 1;
-words[currentWordIndex].style.opacity = "1";
-var rotateText = function () {
-    var currentWord = words[currentWordIndex];
-    var nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-    // rotate out letters of current word
-    Array.from(currentWord.children).forEach(function (letter, i) {
-        setTimeout(function () {
-            letter.className = "letter out";
-        }, i * 80);
-    });
-    // reveal and rotate in letters of next word
-    nextWord.style.opacity = "1";
-    Array.from(nextWord.children).forEach(function (letter, i) {
-        letter.className = "letter behind";
-        setTimeout(function () {
-            letter.className = "letter in";
-        }, 340 + i * 80);
-    });
-    currentWordIndex =
-        currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-};
-rotateText();
-setInterval(rotateText, 4000);
+var o = $(".card");
+$("#top").on("mousemove", function (t) {
+    var e = -($(window).innerWidth() / 2 - t.pageX) / 30,
+        n = ($(window).innerHeight() / 2 - t.pageY) / 10;
+    o.attr("style", "transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-webkit-transform: rotateY(" + e + "deg) rotateX(" + n + "deg);-moz-transform: rotateY(" + e + "deg) rotateX(" + n + "deg)")
+})
